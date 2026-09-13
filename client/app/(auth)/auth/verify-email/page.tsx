90af7955876af39102768a030d5184635ed8cf74
Aurio Rajaa <riorajaa2018@gmail.com>
2026-09-13 09:04:48 +0700

    adding use suspense

--- client/app/(auth)/auth/verify-email/page.tsx
+++ client/app/(auth)/auth/verify-email/page.tsx
@@ -1,7 +1,18 @@
-import {VerifyEmailForm} from "@/components/auth/verify-email-form";
+import { Suspense } from "react";
+import { VerifyEmailForm } from "@/components/auth/verify-email-form";
 
 export default function VerifyEmailPage() {
-    return (
-        <VerifyEmailForm/>
-    );
+  return (
+    <Suspense
+      fallback={
+        <section className="flex items-center relative">
+          <div className="py-10 md:py-20 max-w-lg px-4 sm:px-0 mx-auto w-full">
+            <div className="text-center">Verifying your email...</div>
+          </div>
+        </section>
+      }
+    >
+      <VerifyEmailForm />
+    </Suspense>
+  );
 }
